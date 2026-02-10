04413b586e6d5a90c05e751658fc5273c0640a94
Riley Hardy <54298858+rileyjhardy@users.noreply.github.com>
2026-02-09 22:31:33 -0600

    update configs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -624,10 +624,7 @@ require("lazy").setup({
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
 					if
 						client
-						and client:supports_method(
-							vim.lsp.protocol.Methods.textDocument_documentHighlight,
-							event.buf
-						)
+						and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
 					then
 						local highlight_augroup =
 							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
